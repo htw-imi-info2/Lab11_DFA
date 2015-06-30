2e35b8fbe08333f3d1c479f4b363dcb57b45b006
B Kleinen <kleinen@infrastructure.de>
2015-06-30 18:48:50 +0200

    made fa protected so that examples can be in other packages

--- test/examples/NFATester.java
+++ test/examples/NFATester.java
@@ -5,17 +5,17 @@ import nfa.NFA;
 
 import org.junit.Test;
 
-
 public abstract class NFATester {
-	 NFA fa;
-	 String string;
-	 boolean inLanguage;
+	protected NFA fa;
+	String string;
+	boolean inLanguage;
 
 	public NFATester(String string, boolean inLanguage) {
 		super();
 		this.string = string;
 		this.inLanguage = inLanguage;
 	}
+
 	@Test
 	public void test() {
 		fa.readString(string);
